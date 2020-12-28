7c08206a5ad5ee2de4cbf5275ab1d7fb85c894a8
izumizawa <izumizawa4@gmail.com>
2020-12-28 15:02:53 -0300

    first commit

---
+++ src/test/java/com/matera/conta/ContaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matera.conta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
