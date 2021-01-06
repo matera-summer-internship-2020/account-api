2fde97d3eca03bcab9b848f227d9a3da2ac4cb20
izumizawa <izumizawa4@gmail.com>
2021-01-06 10:03:33 -0300

    fix: add getter and setter in account type

--- src/main/java/com/matera/account/accounttype/AccountType.java
+++ src/main/java/com/matera/account/accounttype/AccountType.java
@@ -9,6 +9,22 @@ public class AccountType {
     public int accountTypeId;
     public String accountType;
 
+    public int getAccountTypeId() {
+        return accountTypeId;
+    }
+
+    public void setAccountTypeId(int accountTypeId) {
+        this.accountTypeId = accountTypeId;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
 }
 
 
