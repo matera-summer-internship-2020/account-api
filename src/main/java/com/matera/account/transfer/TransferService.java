3ab724751c285b6e29979eb408fd2313892e7812
Francisco <franciscogupe@gmail.com>
2021-01-25 14:56:48 -0300

    removed unnecessary imports

--- src/main/java/com/matera/account/transfer/TransferService.java
+++ src/main/java/com/matera/account/transfer/TransferService.java
@@ -8,9 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
